b7a9f68e80bb1b15968ec75cf6e7264dda584a62
ektaj23 <ektarj166@gmail.com>
2026-07-08 12:33:31 +0530

    Add Variables.java

---
+++ Variables.java
@@ -0,0 +1,17 @@
+ public class Variables {
+
+    public static void main(String[] args) {
+
+        int age = 20;
+        double marks = 85.5;
+        char grade = 'A';
+        boolean pass = true;
+        String name = "Ekta";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + grade);
+        System.out.println("Pass: " + pass);
+    }
+}
