77cb76eebbe7da1570fe0e9e90bcd9d308afd7c7
ektaj23 <ektarj166@gmail.com>
2026-07-11 11:28:02 +0530

    Add IfDemo.java

---
+++ IfDemo.java
@@ -0,0 +1,12 @@
+public class IfDemo {
+
+    public static void main(String[] args) {
+
+        int age = 20;
+
+        if (age >= 18) {
+            System.out.println("You are eligible to vote.");
+        }
+
+    }
+}
