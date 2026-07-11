0b7396a1ce6de9a0417bddcb9d3bd2fa2130ad2c
ektaj23 <ektarj166@gmail.com>
2026-07-11 11:34:09 +0530

    Add ElseIfDemo

---
+++ ElseIfDemo.java
@@ -0,0 +1,16 @@
+public class ElseIfDemo {
+    public static void main(String[] args) {
+
+        int marks = 85;
+
+        if (marks >= 90) {
+            System.out.println("Grade A+");
+        } else if (marks >= 75) {
+            System.out.println("Grade A");
+        } else if (marks >= 50) {
+            System.out.println("Grade B");
+        } else {
+            System.out.println("Fail");
+        }
+    }
+}
